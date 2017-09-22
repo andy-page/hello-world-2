e6621a0fcc9f223fb2d0b851deb1a9e93a2b96b4
andy-page <andy.page@oracle.com>
2017-09-22 10:37:49 +0100

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Andy Page friends 3!!!!!"))
+	w.Write([]byte("Hello Andy Page friends 4!!!!!"))
 }
 
 func main() {
